444ec2d01ec286c9087c143abb2936128aeeb07c
Anton Karnachuk <aka@tradeshift.com>
2020-01-24 10:09:52 +0700

    Add smart-html-elements

--- packages/tv-app-web/src/App.svelte
+++ packages/tv-app-web/src/App.svelte
@@ -1,47 +1,45 @@
 <script>
-  import { WiredTab } from "wired-elements";
-  import { WiredInput } from "wired-elements";
-  import { WiredButton } from "wired-elements";
+    import '@smarthtmlelements/smart-elements/source/styles/smart.default.css';
+    import '@smarthtmlelements/smart-elements/source/modules/smart.button.js';
+    import "@smarthtmlelements/smart-elements/source/modules/smart.tabs.js";
 
-  export let name;
-  const channels = fetch("http://localhost:3000/playlist")
-    .then(response => response.json())
-    .then(json => json.channels);
+    const channels = fetch("http://localhost:3000/playlist")
+            .then(response => response.json())
+            .then(json => json.channels);
 
-  let files = [];
-  function sendData() {
-    const formData = new FormData();
-    formData.append("file", files[0]);
-    fetch("http://localhost:3000/playlist", {
-      mode: 'no-cors',
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": undefined
-      },
-      body: formData
-    }).then(result => {
-        return result.json();
-    }).catch(err => {
-        console.error("err_backend" + err);
-    });
-  }
+    let files = [];
+
+    function sendData() {
+        const formData = new FormData();
+        formData.append("file", files[0]);
+        fetch("http://localhost:3000/playlist", {
+            mode: 'no-cors',
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": undefined
+            },
+            body: formData
+        }).then(result => {
+            return result.json();
+        }).catch(err => {
+            console.error(err);
+        });
+    }
 </script>
 
 <div>
-  <wired-tabs selected="tabChannels">
-    <wired-tab name="tabChannels" label="Channels">
-      {#await channels then values}
-        {#each values as channel}
-          <li>{channel.title}</li>
-        {/each}
-      {/await}
-    </wired-tab>
-    <wired-tab name="tabGuide">aaaa</wired-tab>
-  </wired-tabs>
-
-  <form>
-    <input type="file" bind:files/>
-    <wired-button elevation="2" onclick={sendData}>Send</wired-button>
-  </form>
+    <smart-button>Normal</smart-button>
+    aaa!!!!
+    <smart-tabs class='material'>
+        <smart-tab-item label="Tab 1" selected={true}>
+            // Content goes here.
+        </smart-tab-item>
+        <smart-tab-item label="Tab 2">
+            // Content goes here.
+        </smart-tab-item>
+        <smart-tab-item label="Tab 3">
+            // Content goes here.
+        </smart-tab-item>
+    </smart-tabs>
 </div>
